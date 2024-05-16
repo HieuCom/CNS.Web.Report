31419c35b9fc36c2f83534fd6d53e1682ff97399
HieuCom <hieutk97@gmail.com>
2024-05-16 09:38:06 +0700

    change: url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  BASE_URL: 'http://erp.cnsvietnam.com.vn',
-  BASE_API: 'http://erp.cnsvietnam.com.vn/api',
+  BASE_URL: 'http://apps.cnsvietnam.com.vn/demo',
+  BASE_API: 'http://apps.cnsvietnam.com.vn/demo/api',
   APP_ID: 'bc989378a62549e9a918df74b59d3f36'
 };
